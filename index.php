96c6f8af947b359fe13e4cfc10580870bed71f00
alfadila satriatama <97171355+alfadilasatriatama@users.noreply.github.com>
2022-03-30 02:15:09 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<div class="py-9 px-9 bg-gray-900 md:py-14 lg:py-16">
+    <!-- form contact -->
+    
+    <div class="mt-10 sm:mt-0">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div class="md:col-span-1">
+        <div class="px-4 sm:px-0">
+            <h3 class="text-lg font-medium leading-6 text-gray-900">Contact Now</h3>
+            <p class="mt-1 text-sm text-gray-600">I will receive your message and let's work together</p>
+        </div>
+        </div>
+        <div class="mt-5 md:mt-0 md:col-span-2">
+        <form action="" method="POST" target="_blank">
+            <div class="shadow overflow-hidden rounded-sm sm:rounded-md ">
+            <div class="px-4 py-5 bg-white sm:p-6">
+                <div class="grid grid-cols-6 gap-6">
+                    <div class="col-span-6 sm:col-span-3">
+                        <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
+                        <input type="text" name="name" id="name" autocomplete="given-name" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-4">
+                        <label for="email-address" class="block text-sm font-medium text-gray-700">Email address</label>
+                        <input type="text" name="email-address" id="email-address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-4">
+                        <label for="message" class="block text-sm font-medium text-gray-700"> Message </label>
+                        <div class="mt-1">
+                            <textarea id="message" name="message" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="you@example.com"></textarea>
+                        </div>
+                        <p class="mt-2 text-sm text-gray-500">Write your message</p>
+                    </div>
+
+                    <input type="hidden" name="noWa" value="6289687478091">
+                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">WhatsApp</button>
+                    </div>
+                </div>
+            
+            </div> 
+
+            </div>
+        </form>
+        </div>
+    </div>
+    </div>
+</div>
